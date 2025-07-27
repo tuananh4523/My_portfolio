30eef42ae52a7deb3bab350e22dd69acbce4bc3d
John Nguyen <tuananh040523@gmail.com>
2025-07-27 17:54:19 +0700

    No1-2772025

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: ['Font-end Developer', 'TikTok Creator', 'Web Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDeplay: 1000,
+    loop: true
+});
