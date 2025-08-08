d32eeb0dc3ca94ea0a4c9fb5f84a17182cba0a16
tuananh040523@gmail.com <tuananh040523@gmail.com>
2025-08-08 15:14:52 +0700

    no1-882025

--- main.js
+++ main.js
@@ -5,3 +5,26 @@ var typed = new Typed('.text', {
     backDeplay: 1000,
     loop: true
 });
+
+
+const theme = localStorage.getItem('theme');
+if (theme) {
+  document.body.setAttribute('data-theme', theme);
+} else {
+  localStorage.setItem('theme', 'dark');
+}
+
+function toggleTheme() {
+  const sunIcon = document.getElementById('sun_icon');
+  const moonIcon = document.getElementById('moon_icon');
+  const body = document.body;
+  const theme = body.getAttribute('data-theme');
+  body.setAttribute('data-theme', theme === 'dark' ? 'light' : 'dark');
+  if (theme === 'dark') {
+    sunIcon.style.display = 'block';
+    moonIcon.style.display = 'none';
+  } else {
+    sunIcon.style.display = 'none';
+    moonIcon.style.display = 'block';
+  }
+}
